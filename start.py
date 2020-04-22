e42fe4ee309c8234022d3eb5e00b80af43cda44b
Wermuind <63809004+Wermuind@users.noreply.github.com>
2020-04-22 19:16:43 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,10 +1,15 @@
 import datetime
 
-def Intersection(a1,b1,a2,b2):
-    if (a1<b2 and b2<b1) or (a1<a2 and a2<b1) or (a2<b1 and b1<b2) or (a2<a1 and a1<b2) or (a1==a2 and b1==b2):
+from flask import Flask
+from flask import render_template, redirect, url_for, request
+
+
+def intersection(a1, b1, a2, b2):
+    if (a1 < b2 < b1) or (a1 < a2 < b1) or (a2 < b1 < b2) or (a2 < a1 < b2) or (a1 == a2 and b1 == b2):
         return True
     else:
         return False
+
 
 BigStruct = []
 f = open('resource_bookings_input.txt', 'r')
@@ -19,20 +24,20 @@ while j<len(List):
 
    i=Str.find(",")
    StartDateTime = datetime.datetime(int(Str[i+1:i+5]),int(Str[i+6:i+8]),int(Str[i+9:i+11]),int(Str[i+12:i+14]),int(Str[i+15:i+17]),int(Str[i+18:i+20]))
-   
+
    i=Str.rfind(",")
    EndDateTime   = datetime.datetime(int(Str[i+1:i+5]),int(Str[i+6:i+8]),int(Str[i+9:i+11]),int(Str[i+12:i+14]),int(Str[i+15:i+17]),int(Str[i+18:i+20]))
-   
+
    Struct = [ResourceId, StartDateTime, EndDateTime]
    BigStruct.append(Struct)
    j=j+1
 
 def IsResourceAvailable(ResourceId, StartDateTime, EndDateTime):
-    i=0
+    i = 0
     while i<len(BigStruct):
-        if ResourceId==BigStruct[i][0] and Intersection(StartDateTime,EndDateTime,BigStruct[i][1],BigStruct[i][2]):
+        if ResourceId == BigStruct[i][0] and intersection(StartDateTime, EndDateTime, BigStruct[i][1], BigStruct[i][2]):
             return False
-        i=i+1
+        i = i+1
     return True
 
 def FindFreeInterval(StartDateTime, EndDateTime):
@@ -41,22 +46,36 @@ def FindFreeInterval(StartDateTime, EndDateTime):
         if IsResourceAvailable(i[0], StartDateTime, EndDateTime)==False:
             continue
         FreeResources.append(i[0])
-    print("Free Resources list:")
     return list(set(FreeResources))
 
-from flask import Flask, render_template
+
 app = Flask(__name__)
+results = []
 
-@app.route("/")
-def start():
-    return render_template('Booking.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+def start_page():
+    return render_template('booking.html', results=results)
+
+
+@app.route('/main', methods=['GET'])
 def main():
-      if request.method == 'POST':
-           StartDateTime = request.form['StartDateTime']
-           EndDateTime = request.form['EndDateTime']
-           Results = FindFreeInternal(StartDateTime, EndDateTime)
-           return render_template('Booking.html', Results=Results)
-      else:
-           return render_template('Booking.html')
+    return render_template('booking.html', results=results)
+
+
+@app.route('/table')
+def table():
+    return render_template('table.html')
+
+
+@app.route('/add_result', methods=['POST'])
+def add_result():
+    a = datetime.datetime(int(request.form['StartDateTime'][0:4]), int(request.form['StartDateTime'][5:7]), int(request.form['StartDateTime'][8:10]), 0, 0, 0)
+    b = datetime.datetime(int(request.form['EndDateTime'][0:4]), int(request.form['StartDateTime'][5:7]), int(request.form['StartDateTime'][8:10]), 23, 59, 59)
+    global results
+    results = ', '.join(FindFreeInterval(a, b))
+    return redirect(url_for('main'))
+
+
+if __name__ == '__main__':
+    app.run()
